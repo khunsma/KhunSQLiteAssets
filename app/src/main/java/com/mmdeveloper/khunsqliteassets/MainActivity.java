89541458084241b3cc6a9344015dbf0b831a8a9c
khunsma <khunsoemoe.aung@kbzbank.com>
2020-01-16 16:26:24 +0630

    Ready to upload on Git

---
+++ app/src/main/java/com/mmdeveloper/khunsqliteassets/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mmdeveloper.khunsqliteassets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
